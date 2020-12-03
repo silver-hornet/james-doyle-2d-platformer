23498b997ac98da4c359508838900facaf93c2f8
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2020-12-03 08:09:28 -0500

    Completed Lesson 32

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance; // Set up a singleton
+
+    public int currentHealth, maxHealth;
+
+    void Awake()
+    {
+        instance = this; // Singleton, ensures there is only ever one PlayerHealthController
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0; // To protect against any weird errors that might happen if currentHealth somehow goes below 0
+            gameObject.SetActive(false);
+        }
+
+        UIController.instance.UpdateHealthDisplay();
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    [SerializeField] Image heart1, heart2, heart3;
+    [SerializeField] Sprite heartFull, heartEmpty;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthDisplay()
+    {
+        switch (PlayerHealthController.instance.currentHealth)
+        {
+            case 3:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartFull;
+                break;
+            case 2:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartFull;
+                heart3.sprite = heartEmpty;
+                break;
+            case 1:
+                heart1.sprite = heartFull;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+            case 0:
+                heart1.sprite = heartEmpty;
+                heart2.sprite = heartEmpty;
+                heart3.sprite = heartEmpty;
+                break;
+        }
+    }
+}
